e09e65212ece98627a4f7ab706ec9f617325b548
CoderHritik0 <hritiksanas47@gmail.com>
2025-10-20 21:23:00 +0530

    Add initial migration and models for notes and note images

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import note, note_image
 
 # Register your models here.
+admin.site.register(note)
+admin.site.register(note_image)
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class note(models.Model):
+    note_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField()
+    tag = models.CharField(max_length=50, blank=True)
+    color = models.CharField(max_length=7, default="#ffffff")
+    is_hidden = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.note_id}';
+
+class note_image(models.Model):
+    image_id = models.AutoField(primary_key=True)
+    note = models.ForeignKey(note, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="notes/images/", max_length=None)
+
+    def __str__(self):
+        return f"Image {self.image_id} for Note_ID: {self.note.note_id}"
